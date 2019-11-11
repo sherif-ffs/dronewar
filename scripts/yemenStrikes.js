75c1394a9380f923aaf0a4ed88b9ac9396c30652
Sherif Elmetwally <sge5@njit.edu>
2019-11-11 15:19:27 -0500

    add strikes for afghanistan, pakistan, and somalia

---
+++ scripts/yemenStrikes.js
@@ -0,0 +1,26 @@
+var xmlhttp = new XMLHttpRequest();
+let myObj;
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      myObj = JSON.parse(this.responseText);
+      let droneStrikes = myObj.AllUSactions;
+      console.log(droneStrikes);
+      let totalDeaths = 0;
+      let totalCivilianDeaths = 0;
+      for (let i=0; i<droneStrikes.length; i++) {
+          let currentObject = droneStrikes[i];
+          totalCivilianDeaths += parseInt(currentObject["Maximum civilians reported killed"])
+          totalDeaths += parseInt(currentObject["Maximum people killed"]);
+      }
+      console.log('totalDeaths: ', totalDeaths);
+      console.log('totalCivilianDeaths: ', totalCivilianDeaths);
+  
+    }
+  };
+  
+  xmlhttp.open("GET", "strikes/yemenStrikes.json", true);
+  xmlhttp.send();
+
+
+
+
