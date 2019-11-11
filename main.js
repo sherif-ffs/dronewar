047c777d797f943f450146a3ae4754a71db386ab
Sherif Elmetwally <sge5@njit.edu>
2019-11-10 22:26:05 -0500

    sad math

--- main.js
+++ main.js
@@ -3,10 +3,22 @@ let myObj;
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     myObj = JSON.parse(this.responseText);
-    console.log(myObj.AllUSactions[0]["Type of attack"]); // Drone Strike
-  }
+    let droneStrikes = myObj.AllUSactions;
+    console.log(droneStrikes);
+    let totalDeaths = 0;
+    let totalCivilianDeaths = 0;
+    for (let i=0; i<droneStrikes.length; i++) {
+        let currentObject = droneStrikes[i];
+        totalCivilianDeaths += parseInt(currentObject["Maximum civilians reported killed"])
+        totalDeaths += parseInt(currentObject["Maximum people killed"]);
+    }
+    console.log('totalDeaths: ', totalDeaths);
+    console.log('totalCivilianDeaths: ', totalCivilianDeaths);
 
+  }
 };
+
 xmlhttp.open("GET", "strikes.txt", true);
 xmlhttp.send();
+
 
