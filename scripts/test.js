c3851755ea3dab1015ca2c84c14e13f7976a4eee
Sherif Elmetwally <sge5@njit.edu>
2019-11-14 15:22:28 -0500

    pull values from form

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('button');
 let submitValue;
+let products;
 button.addEventListener('click', (e) => {
     let childNodes = e.target.parentElement.childNodes;
     submitValue = childNodes[11].lastElementChild.value;
@@ -11,25 +12,40 @@ button.addEventListener('click', (e) => {
     let to = document.querySelector('.to').value;
     let country = document.querySelector('.country').value;
 
-    console.log('displayPeopleKilled: ', displayPeopleKilled);
-    console.log('displayPeopleInjured: ', displayPeopleInjured);
-    console.log('displayNumberOfStrikes: ', displayNumberOfStrikes);
-    console.log('from date: ', from);
-    console.log('to date: ', to);
-    console.log('selected country: ', country)
+    // console.log('displayPeopleKilled: ', displayPeopleKilled);
+    // console.log('displayPeopleInjured: ', displayPeopleInjured);
+    // console.log('displayNumberOfStrikes: ', displayNumberOfStrikes);
+    // console.log('from date: ', from);
+    // console.log('to date: ', to);
+    // console.log('selected country: ', country)
 
     if (country === 'yemen') {
         fetch(`strikes/yemenStrikes.json`).then(function(response) {
             return response.json();
           })
           .then(function(json) {
-            products = json.yemen;
-            console.log(`${country} strikes`, products);
-            // initialize()
+            let products = json.yemen;
+            if (displayNumberOfStrikes) {
+                let minimumStrikes = 0;
+                let maximumStrikes = 0;
+                for (let i=0; i<products.length; i++) {
+                    minimumStrikes += parseInt(products[i]['Minimum number of strikes']);
+                    maximumStrikes += parseInt(products[i]['Maximum number of strikes']);
+                }
+                console.log(`${minimumStrikes} - ${maximumStrikes}`)
+            }
+            if (displayPeopleInjured) {
+                console.log('displayPeopleInjured: ', displayPeopleInjured);
+            }
+            if (displayPeopleKilled) {
+                console.log('displayPeopleKilled: ', displayPeopleKilled);
+            }
+
           })
           .catch(function(err) {
             console.log('Fetch problem: ' + err.message);
           });
+
     }
     
 });
